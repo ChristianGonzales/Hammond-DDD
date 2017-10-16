2205784233f60a80f6b5291940c5af040c90bb6e
Christian Gonzales <christian.gonzales@selu.edu>
2017-10-16 11:56:42 -0500

    Added ContactActivity and worked on navigation

--- HammondDDD/app/src/main/java/edu/selu/cgonzales/hammondddd/ContactActivity.java
+++ HammondDDD/app/src/main/java/edu/selu/cgonzales/hammondddd/ContactActivity.java
@@ -1,18 +1,15 @@
 package edu.selu.cgonzales.hammondddd;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+public class ContactActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
+                    openHome();
                     return true;
                 case R.id.navigation_map:
                     openMap();
                     return true;
                 case R.id.navigation_contact:
-                    openContact();
+//                    mTextMessage.setText(R.string.title_contact);
                     return true;
             }
             return false;
@@ -36,33 +34,32 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private Toolbar.OnMenuItemClickListener menuItemClickListener =
-        new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.action_search:
-                        System.out.println("Search");
-                        return true;
+            new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.action_search:
+                            System.out.println("Search");
+                            return true;
 //                    case R.id.action_contact:
 //                        System.out.println("Contact");
 //                        return true;
-                    case R.id.action_settings:
-                        System.out.println("Settings");
-                        return true;
+                        case R.id.action_settings:
+                            System.out.println("Settings");
+                            return true;
+                    }
+                    return false;
                 }
-                return false;
-            }
-        };
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contact);
 
         Toolbar parentToolbar = (Toolbar) findViewById(R.id.parent_toolbar);
         setSupportActionBar(parentToolbar);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
         parentToolbar.setOnMenuItemClickListener(menuItemClickListener);
@@ -75,13 +72,14 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void openHome(){
+        Intent homeIntent = new Intent(this, MainActivity.class);
+        startActivity(homeIntent);
+    }
+
     public void openMap(){
         Intent openMapIntent = new Intent(this, MapActivity.class);
         startActivity(openMapIntent);
     }
 
-    public void openContact(){
-        Intent openContactIntent = new Intent(this, ContactActivity.class);
-        startActivity(openContactIntent);
-    }
 }
